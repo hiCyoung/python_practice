1539a505e91980d7a4311d7e3b885a9e480979a7
hiCyoung <328491755@qq.com>
2013-06-22 11:24:03 +0800

    函数

--- P1/func.py
+++ P1/func.py
@@ -11,7 +11,6 @@ def sayHello():
 def defaultValue(x,y=3):  #不能是(x=3,y)
          print("x=",x," y=",y)
          return x**2+y**3
-
 print(f(1,2))
 sayHello()
 print(3**3)
@@ -19,3 +18,10 @@ print(defaultValue(2))
 print(defaultValue(2,1))
 print(defaultValue(y=2,x=3)) #关键字，破除了函数参数定义顺序的限制
 print(defaultValue(x=2,y=3))
+###递归函数##
+def jc(n):
+    if n ==1:
+        return 1
+    else:
+        return n*jc(n-1)
+print(jc(5))
