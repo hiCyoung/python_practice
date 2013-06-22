e881252c23bbfa4aa670739995c2311ca7cfbf62
hiCyoung <328491755@qq.com>
2013-06-22 12:00:12 +0800

    函数

--- P1/func.py
+++ P1/func.py
@@ -25,3 +25,36 @@ def jc(n):
     else:
         return n*jc(n-1)
 print(jc(5))
+
+###匿名函数##
+user=[
+    ("tom",20,1000),
+    ("aom",24,1900),
+    ("vom",23,1800),
+    ("com",21,1400),
+    ("bom",25,1700),
+    ]
+# user.sort() //默认按第一个元素排序
+# user.sort(key=lambda x:x[2]) #按第二个元素排序
+#以上等同于
+# def f1(cc):
+#     return cc[1]
+# user.sort(key=f1)
+print(user)
+
+##generator生成器
+#在 python 中，generator function 是一个可以保存状态的函数，
+# 用 yield 指令(不是 return )返回一个值，保存当前整个函数执行状态，等待下一次调用，
+# 当需要该函数返回值时，该 generator function 恢复整个函数执行状态，
+# 直至再次遇到 yield 指令，再次返回一个值，再次保存当前函数执行状态，再次等待。
+# 如此循环往复，直至函数末尾，发生　StopIteration　异常。
+def gn():
+    for i in list(range(10)):
+        yield  i*2
+
+g=gn()
+print(g.__next__())
+print(g.__next__())
+print(g.__next__())
+print(g.__next__())
+print(g.__next__())
